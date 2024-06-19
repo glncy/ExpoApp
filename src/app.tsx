c463a750bd0712b0be56aebbe2ad7cea38a74b9a
Glency Tirao <glncy@Glencys-MacBook-Pro.local>
2024-06-19 17:20:27 +0800

    chore: add theming

--- src/app.tsx
+++ src/app.tsx
@@ -7,21 +7,12 @@ import "@/src/theme/unistyles";
 import notifee from "@notifee/react-native";
 import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
 import messaging from "@react-native-firebase/messaging";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import { Theme, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { ReactNode, useEffect, useState } from "react";
-import {
-  AppState,
-  AppStateStatus,
-  Platform,
-  useColorScheme,
-} from "react-native";
+import { AppState, AppStateStatus, Platform } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SWRConfig } from "swr";
 
@@ -37,6 +28,8 @@ import {
   useNavigationPersistence,
 } from "@/src/utils/navigation";
 import * as storage from "@/src/utils/storage";
+import { NAV_THEME } from "@/src/utils/tailwind/constants";
+import { useColorScheme } from "@/src/utils/tailwind/useColorScheme";
 
 export const NAVIGATION_PERSISTENCE_KEY = "NAVIGATION_STATE";
 
@@ -67,6 +60,18 @@ if (Platform.OS !== "web") {
   });
 }
 
+// Theming
+const THEME_STORAGE_KEY = "APP_THEME";
+const LIGHT_THEME: Theme = {
+  dark: false,
+  colors: NAV_THEME.light,
+};
+
+const DARK_THEME: Theme = {
+  dark: true,
+  colors: NAV_THEME.dark,
+};
+
 const App = ({ children }: { children: ReactNode }) => {
   const [isReady, setIsReady] = useState(false);
   const [isNetworkLoaded, setIsNetworkLoaded] = useState(false);
@@ -74,7 +79,8 @@ const App = ({ children }: { children: ReactNode }) => {
   const { network } = useAppStore();
   const { isInternetReachable } = useNetInfo();
   const [isFontsLoaded] = useFonts(fontsToLoad);
-  const colorScheme = useColorScheme();
+  const { colorScheme, setColorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = useState(false);
   const { isRestored: isNavigationStateRestored } = useNavigationPersistence(
     storage,
     NAVIGATION_PERSISTENCE_KEY
@@ -83,11 +89,34 @@ const App = ({ children }: { children: ReactNode }) => {
   // handle back button
   useBackButtonHandler((routeName) => Config.exitRoutes.includes(routeName));
 
+  // handle color scheme
   useEffect(() => {
-    if (isFontsLoaded && isNetworkLoaded) {
+    (async () => {
+      const theme = storage.loadString(THEME_STORAGE_KEY);
+      if (Platform.OS === "web") {
+        // Adds the background color to the html element to prevent white background on overscroll.
+        document.documentElement.classList.add("bg-background");
+      }
+      if (!theme) {
+        storage.saveString(THEME_STORAGE_KEY, colorScheme);
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      const colorTheme = theme === "dark" ? "dark" : "light";
+      if (colorTheme !== colorScheme) {
+        setColorScheme(colorTheme);
+        setIsColorSchemeLoaded(true);
+        return;
+      }
+      setIsColorSchemeLoaded(true);
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (isFontsLoaded && isNetworkLoaded && isColorSchemeLoaded) {
       setIsReady(true);
     }
-  }, [isFontsLoaded, isNetworkLoaded]);
+  }, [isFontsLoaded, isNetworkLoaded, isColorSchemeLoaded]);
 
   useEffect(() => {
     if (isNavigationStateRestored && isReady) {
@@ -161,9 +190,7 @@ const App = ({ children }: { children: ReactNode }) => {
             },
           }}
         >
-          <ThemeProvider
-            value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-          >
+          <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
             {children}
             <StatusBar style="auto" />
           </ThemeProvider>
